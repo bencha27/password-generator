0cdb41c7793527615f61b0e7a1b2643d116d6210
bencha27 <115957643+bencha27@users.noreply.github.com>
2022-12-26 10:44:35 -0700

    Fixed error in script.js of criteria prompts after initial prompt is cancelled; added description and usage to README

--- assets/script.js
+++ assets/script.js
@@ -16,48 +16,48 @@ function writePassword() {
   } else if (passwordLength > 128) {
     alert("Your password cannot be more than 128 characters long.");
     writePassword();
-  }
-
-  // Confirms for lowercase, uppercase, numbers, special characters
-  var chooseLowerCase = confirm("Do you want to include lowercase letters in your password?");
-  var chooseUpperCase = confirm("Do you want to include uppercase letters in your password?");
-  var chooseNum = confirm("Do you want to include numbers in your password?");
-  var chooseSpecChar = confirm("Do you want to include special characters in your password?");
-  if (!chooseLowerCase && !chooseUpperCase && !chooseNum && !chooseSpecChar) {
-    alert("You must choose at least one character type.")
-    writePassword();
-  }
-
-  // Array of characters to create password from
-  var passwordChar = [];
-  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"/';
-
-  // Add chosen characters to array of characters
-  if (chooseLowerCase) {passwordChar += lowerCaseChar}
-  if (chooseUpperCase) {passwordChar += upperCaseChar}
-  if (chooseNum) {passwordChar += numbers};
-  if (chooseSpecChar) {passwordChar += specialChar};
-
-  var passwordArray = passwordChar.split("");
-  var randomPassword = "";
-
-  // Choose random characters from array and add to password
-  function generatePassword(number) {
-    for (var i = 0; i < number; i++) {
-      var index = Math.floor(Math.random() * (passwordArray.length - 1));
-      randomPassword += passwordArray[index];
-      console.log(i);
+  } else {
+    // Confirms for lowercase, uppercase, numbers, special characters
+    var chooseLowerCase = confirm("Do you want to include lowercase letters in your password?");
+    var chooseUpperCase = confirm("Do you want to include uppercase letters in your password?");
+    var chooseNum = confirm("Do you want to include numbers in your password?");
+    var chooseSpecChar = confirm("Do you want to include special characters in your password?");
+    if (!chooseLowerCase && !chooseUpperCase && !chooseNum && !chooseSpecChar) {
+      alert("You must choose at least one character type.")
+      writePassword();
     }
-    return randomPassword;
-  };
-
-  // Render the password on the page
-  var password = generatePassword(passwordLength);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  
+    // Array of characters to create password from
+    var passwordChar = [];
+    var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+    var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"/';
+  
+    // Add chosen characters to array of characters
+    if (chooseLowerCase) {passwordChar += lowerCaseChar}
+    if (chooseUpperCase) {passwordChar += upperCaseChar}
+    if (chooseNum) {passwordChar += numbers};
+    if (chooseSpecChar) {passwordChar += specialChar};
+  
+    var passwordArray = passwordChar.split("");
+    var randomPassword = "";
+  
+    // Choose random characters from array and add to password
+    function generatePassword(number) {
+      for (var i = 0; i < number; i++) {
+        var index = Math.floor(Math.random() * (passwordArray.length - 1));
+        randomPassword += passwordArray[index];
+        console.log(i);
+      }
+      return randomPassword;
+    };
+  
+    // Render the password on the page
+    var password = generatePassword(passwordLength);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
